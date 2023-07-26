adf9137355349027338610da54b8f3b1aaf7d6b8
Vyacheslav Minaev <slavik.minaeff@yandex.ru>
2023-07-26 22:38:48 +0300

    Сделал второе задание

--- Program.cs
+++ Program.cs
@@ -3,22 +3,72 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Введите первое число :");
-int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число :");
+// int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число :");
- int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число :");
+//  int numberB = Convert.ToInt32(Console.ReadLine());
 
- if(numberA > numberB )
- {
-     Console.Write (numberA);
- }
+//  if(numberA > numberB )
+//  {
+//      Console.Write (numberA);
+//  }
 
- else if (numberB > numberA )
- {
-     Console.Write(numberB);
- }
- else 
- {
-     Console.Write("Числа равны");
-}
+//  else if (numberB > numberA )
+//  {
+//      Console.Write(numberB);
+//  }
+//  else 
+//  {
+//      Console.Write("Числа равны");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.Write("Введите первое число :");
+//  int numberA = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число :");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число :");
+// int numberC = Convert.ToInt32(Console.ReadLine());
+
+// if(numberA > numberB)
+// {
+//     Console.Write(numberA);
+// }
+
+// else if (numberA > numberC)
+// {
+//     Console.Write(numberA);
+// }
+
+// else if (numberB > numberA)
+// {
+//     Console.Write(numberB);
+// }
+
+// else if (numberB > numberC)
+// {
+//     Console.Write(numberB);
+// }
+
+// else if (numberC > numberA)
+// {
+//     Console.Write(numberC);
+// }
+
+// else if (numberC > numberB)
+// {
+//     Console.Write(numberC);
+// }
+
+// else
+// {
+//     Console.Write("Числа равны");
+// }
