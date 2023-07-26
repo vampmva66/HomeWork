495e93008773b6ed26130218152095aafadbf3d2
Vyacheslav Minaev <slavik.minaeff@yandex.ru>
2023-07-26 23:06:47 +0300

    Решил последнюю задачу

--- Program.cs
+++ Program.cs
@@ -89,3 +89,18 @@
 // {
 //     Console.Write("Нет");
 // }
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while(i <= number)
+{
+    Console.WriteLine(i);
+    i += 2;
+}
